2f96ef6ced14893cd53158977a07eaabdef58c9d
zoeang <36051346+zoeang@users.noreply.github.com>
2018-03-02 16:53:30 -0600

    Fix trapezoid function
    TO conform to instructions

--- integrateIt/R/trapezoid.R
+++ integrateIt/R/trapezoid.R
@@ -1,7 +1,11 @@
 library(zoo)
-Trapezoid<- function(frame){
-            x <- frame[,1]
-            y <- frame[,2]
-            id <- order(x)
-            AUC <- sum(diff(x[id])*abs(rollmean(y[id],2)))
-            }
+trapezoid<- function(x, y, a, b, rule){
+  n<-length(x)
+  a<-min(x)
+  b<-max(x)
+  h<- (b-a)/n
+  midx<-3:length(x)-1
+  id <- order(x)
+  AUC<- h/2*(y[1]+2*sum(y[midx])+y[length(y)])
+  print(c(x,y,AUC, rule))
+}
