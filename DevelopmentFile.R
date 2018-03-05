ecf3c397e9ae9c559a098899a4f9b749e4089bb3
zoeang <36051346+zoeang@users.noreply.github.com>
2018-03-05 16:54:38 -0600

    Tested the integrateit function
    it seems to work

--- DevelopmentFile.R
+++ DevelopmentFile.R
@@ -2,13 +2,13 @@
 ## Load libraries and set working directory
 library(devtools)
 library(roxygen2)
-setwd("C:\Users\zoeja\OneDrive\Documents\Spring2018\R\PS5") #This will need to be changed to match your directory
+setwd("C:/Users/zoeja/OneDrive/Documents/Spring2018/R") #This will need to be changed to match your directory
 
-
+getwd()
 ## This is run once when the package strcuture is first created
 
-create(integrateIt)
-document()
+#create(integrateIt)
+#document()
 ## This can be run many times as the code is updates
 current.code <- as.package("integrateIt")
 load_all(current.code)
@@ -21,7 +21,9 @@ x<-c(1,2)
 y<-c(3,4)
 squaresObject <- addSquares(x, y)
 squaresObject
-
+x<-seq(1,9,1)
+y<-c(2,3,7,3,800,8,9,4,1)
+  integrateIt(x,y,a,b, "simpsons")
 
 
 
